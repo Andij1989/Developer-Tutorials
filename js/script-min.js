e1766243ca6234af119584a9fe2afc8816f762c4
Andreas Jeske <andreas.jeske@wambo-agency.com>
2018-09-25 11:42:19 +0200

    added js-code

---
+++ js/script-min.js
@@ -0,0 +1,1 @@
+function buttonAction(){document.getElementById("textChange").innerHTML="Dieser Text wurde verändert!"}function iconChange(){document.getElementById("thumb").className="fas fa-thumbs-down"}
---
+++ js/script.js
@@ -0,0 +1,6 @@
+function buttonAction() {
+    document.getElementById("textChange").innerHTML = "Dieser Text wurde verändert!";
+}
+function iconChange() {
+    document.getElementById("thumb").className = "fas fa-thumbs-down";
+}
